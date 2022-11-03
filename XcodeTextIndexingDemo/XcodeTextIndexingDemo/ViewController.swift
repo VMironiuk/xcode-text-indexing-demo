dd7a6fcd9ca2b69db61538765752dad7e2b17104
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2022-11-03 13:53:05 +0200

    Add the project

---
+++ XcodeTextIndexingDemo/XcodeTextIndexingDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XcodeTextIndexingDemo
+//
+//  Created by Volodymyr Myroniuk on 03.11.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
